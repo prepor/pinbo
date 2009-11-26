35d9a4eb1a4f82a3384e4c8682b9bf3062faf34b
Andrew Rudenko <ceo@prepor.ru>
2009-11-27 00:41:25 +0300

    Fix for Passenger

--- lib/pinbo/middleware.rb
+++ lib/pinbo/middleware.rb
@@ -5,7 +5,7 @@ module Pinbo
     end
   
     def call(env)
-      Pinbo.start :script_name => env['REQUEST_PATH']
+      Pinbo.start :script_name => env['PATH_INFO']
       res = @app.call(env)
       Pinbo.stop :status => res[0], :server_name => env['SERVER_NAME'], :document_size => res[1]['Content-Length'].to_i
       res
